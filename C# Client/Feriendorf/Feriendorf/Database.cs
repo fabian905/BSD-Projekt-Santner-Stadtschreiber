44c7a0159bb00d48487435ee50114f6550ae8ade
fabian905 <fabi.stadtschreiber@gmail.com>
2017-10-27 08:30:22 +0200

    Structure fix

---
+++ C# Client/Feriendorf/Feriendorf/Database.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feriendorf
+{
+    public class Database
+    {
+        private static OleDbConnection myConnection = null;
+
+        public string Connect()
+        {
+            try
+            {
+                string connectionString = "Provider=OraOLEDB.Oracle; Data Source = 192.168.128.152/ora11g; User Id = d5a17; Password = d5a; OLEDB.NET = True; ";
+
+                myConnection = new OleDbConnection(connectionString);
+                myConnection.Open();
+                return "CONNECTED!";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public void Close()
+        {
+            myConnection.Close();
+        }
+
+        public OleDbConnection GetConnection()
+        {
+            return myConnection;
+        }
+
+        public OleDbDataReader ExecuteCommand(string sql)
+        {
+            OleDbCommand cmd = myConnection.CreateCommand();
+            cmd.CommandText = sql;
+            OleDbDataReader reader = cmd.ExecuteReader();
+            return reader;
+        }
+    }
+}
---
+++ C# Client/Feriendorf/Feriendorf/EinzelansichtDefekt.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Feriendorf
+{
+    /// <summary>
+    /// Interaction logic for EinzelansichtDefekt.xaml
+    /// </summary>
+    public partial class EinzelansichtDefekt : Window
+    {
+        public EinzelansichtDefekt()
+        {
+            InitializeComponent();
+        }
+
+        private void bttnBeheben_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSchließen_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
